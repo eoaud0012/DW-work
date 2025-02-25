2e53d1178449b9532f23565334bb98538619a896
eoaud0012 <eoaud0012@naver.com>
2025-02-25 13:09:16 +0900

    Update automation17-10.py

--- 작업/automation17-10.py
+++ 작업/automation17-10.py
@@ -226,6 +226,8 @@ def scrape_articles():
     # 필요 시 헤드리스 모드 사용
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument(
         "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
         "AppleWebKit/537.36 (KHTML, like Gecko) "
